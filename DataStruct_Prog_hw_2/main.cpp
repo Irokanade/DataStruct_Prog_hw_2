608ad1a9bc400dfdcde665d056a79ac7ccb90c3a
Irokanade <michaeljs.leong@gmail.com>
2022-10-18 11:40:46 +0800

    Initial Commit

---
+++ DataStruct_Prog_hw_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataStruct_Prog_hw_2
+//
+//  Created by Michael Leong on 18/10/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
